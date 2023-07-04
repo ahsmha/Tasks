d7defc9189ba8c199e6d95abd6c2e0f8f4b41df9
ahsmha <ahmed.5wna0@aleeas.com>
2023-07-04 12:33:29 +0530

    handlers
    Signed-off-by: ahsmha <ahmed.5wna0@aleeas.com>

--- handler/note_handler.go
+++ handler/note_handler.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"ahsmha/notes/usecase"
+)
+
+type NoteHandler struct {
+	noteUsecase usecase.NoteUsecase
+}
+
+func NewNoteHandler(noteUsecase usecase.NoteUsecase) NoteHandler {
+	return NoteHandler{
+		noteUsecase: noteUsecase,
+	}
+}
