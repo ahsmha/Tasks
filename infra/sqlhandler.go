7600dbe868012fa5c4f0b1dc3f90600970c28dab
ahsmha <ahmed.5wna0@aleeas.com>
2023-07-04 12:12:05 +0530

    update sqlhandler
    Signed-off-by: ahsmha <ahmed.5wna0@aleeas.com>

--- infra/sqlhandler.go
+++ infra/sqlhandler.go
@@ -1,1 +1,35 @@
 package infra
+
+import "github.com/jmoiron/sqlx"
+
+type SqlHandler struct {
+	Conn *sqlx.DB
+}
+
+func NewSqlHandler() *SqlHandler {
+	// Data Source Name
+	dsn, err := dsn()
+	if err != nil {
+		panic(err.Error)
+	}
+
+	db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		panic(err.Error)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err.Error)
+	}
+
+	return &SqlHandler{Conn: db}
+}
+
+func ConnectDB() (*sqlx.DB, error) {
+	handler := NewSqlHandler()
+
+	return handler.Conn, nil
+}
+
+func dsn() (string, error) {
+	return "", nil
+}
